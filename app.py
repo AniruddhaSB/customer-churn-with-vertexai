c4f72a722b6dd346654b4b35f69710e0fbfd7d35
aniruddhasb <aniruddhasb@gmail.com>
2025-11-10 21:36:16 +0530

    Added code to load data from multiple CSV files from GCS path

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import glob
 import os
+import pandas as pd
 from flask import Flask, request
 from datetime import datetime
+from dotenv import load_dotenv
 
-import pandas as pd
+from load_data import load_data
+
+
 
 app = Flask(__name__)
 
@@ -21,8 +25,24 @@ def hello_world():
 
     return response
 
+@app.route('/load_data', methods=['GET'])
+def load_data_api():
+
+
+    raw_data = load_data(
+        project_id=os.getenv("PROJECT_ID", "nimble-octagon-253816"),
+        bucket_name=os.getenv("BUCKET_NAME", "customer-churn-demo"),
+        raw_data_folder_path=os.getenv("RAW_DATA_FOLDER_PATH", "gs://customer-churn-demo/data/raw/")
+    )
+    if raw_data.empty is True:
+        return "No data loaded."
+    else:
+        return f"Loaded {len(raw_data)} records."
+    
+    
 # This block must be at the same level of indentation as the import statement and app = Flask(__name__)
 if __name__ == '__main__':
-    # Get the port from the environment variable, defaulting to 5000 if not found
+    # Get the port from the environment variable, defaulting to 8080 if not found
     port = int(os.environ.get('PORT', 8080))
+    load_dotenv()
     app.run(host='0.0.0.0', port=port, debug=True)
